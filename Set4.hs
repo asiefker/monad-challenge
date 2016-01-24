e1963218f31604bb1cf4fc6fe920b9ee0349afe6
Adam Siefker <siefker@gmail.com>
2016-01-24 10:41:48 -0800

    Everything but sequence
    Maybe and Gen are duplicated. Will need to clean up to support final
    execise.

--- Set4.hs
+++ Set4.hs
@@ -22,10 +22,36 @@ instance Monad Maybe  where
     bind Nothing _ = Nothing
     bind (Just a) f= f a
 
+instance Show a => Show (Maybe a) where 
+        show (Just a) = "Just " ++ show a
+        show Nothing = "Nothing"
+
 instance Monad (Gen s) where 
     return a = Gen (\ s -> (a,s))
     bind ma f = Gen $ \s -> let (a', s') = run ma s 
                                  in run (f a') s'
+-- Rewritten from ealier
+-- repRandom
+--sequence:: Monad m => [m a] -> m [a]
+--sequence mas = return $ foldl (\ as ma -> bind ma (:as)) [] mas 
 
-ylink:: Monad m => (a->b->c)-> m a -> m b -> m c 
-ylink f ma mb = bind ma (\ a -> bind mb  (return . f a) )
+-- chain
+(=<<):: Monad m => (a -> m b) -> m a -> m b
+(=<<) f ma = bind ma f 
+
+-- ylink/gernalB/allPerms
+liftM2:: Monad m => (a->b->c)-> m a -> m b -> m c 
+liftM2 f ma mb = bind ma (\ a -> bind mb  (return . f a) )
+
+liftM3:: Monad m => (a -> b -> c -> d) -> m a -> m b -> m c -> m d
+liftM3 f ma mb mc =  return f `ap` ma `ap` mb `ap` mc  
+
+-- permStep
+ap :: Monad m => m (a -> b) -> m a -> m b
+ap mf ma = bind mf (\f -> bind ma (return . f))
+
+-- combine, but I didn't have a combine
+join :: Monad m => m (m a) -> m a
+join m = bind m id 
+
+
